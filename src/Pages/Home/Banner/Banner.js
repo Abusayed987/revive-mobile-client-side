65fbde646f80cdabbe23acb9c22c7c1c243422be
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-11 15:49:40 +0600

    create default NavBar

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="hero" style={{ backgroundImage: `url("https://i.ibb.co/WPxXJjt/revive-mobile-banner.jpg")` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <img src="https://i.ibb.co/WPxXJjt/revive-mobile-banner.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Banner;  
